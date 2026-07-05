611ae132ec3d9734f032d74421739274eeea2fe6
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-07-05 15:32:18 +0500

    Created 2 and 3 d arrays and  acceess the values

--- dimensions_NP.py
+++ dimensions_NP.py
@@ -1,27 +1,60 @@
 import numpy as np
 
-# ZERO DIMESNIONAL ARRAYS
+# # ZERO DIMESNIONAL ARRAYS
 
-array=np.array("A")
-print(array.ndim)
+# array=np.array("A")
+# print(array.ndim)
 
-# ONE DIMESNIONAL ARRAYS
+# # ONE DIMESNIONAL ARRAYS
 
-array=np.array(["A","B","C"])
+# array=np.array(["A","B","C"])
 
-print(array.ndim)
+# print(array.ndim)
 
-# TWO DIMENSIONAL ARRAYS
+# # TWO DIMENSIONAL ARRAYS
 
-array = np.array([["A","B","C"],
-                 ["D","E","F"],])
+# array = np.array([["A","B","C"],
+#                  ["D","E","F"],])
 
-print(array.ndim)
+# print(array.ndim)
 
-# THREE DIMENSIONALS ARRAYS
+# # THREE DIMENSIONALS ARRAYS
 
-array = np.array([[["A","B"],
-                   ["C","D"],
-                   ["E","F"]]])
+# array = np.array([[["A","B"],
+#                    ["C","D"],
+#                    ["E","F"]]])
 
-print(array.ndim)
+# print(array.ndim)
+
+#<-------------------------------------------------------------------------------------------->
+
+#Multi Dimensional Indexing. Getting values from he arrays
+
+# 2 Dimensional Array
+array =np.array([
+    ["A","B","C","D"],
+    ["E","F","G","H"],
+    ["I","J","K","L"],
+    ["M","N","O","P"],
+    ["Q","R","S","T"],
+    ["U","V","W","X"]])
+
+#print(array(rows,columns))
+print(array[5,3])
+
+# 3Dimensional Array
+
+Three_D_array=np.array([[
+    ["A","B"],
+    ["C","D"],
+    ["K","L"]
+],
+[
+    ["E","F"],
+    ["G","H"],
+    ["I","J"],
+
+]
+])
+print(np.shape(Three_D_array))
+
