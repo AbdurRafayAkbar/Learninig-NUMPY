bba7e532bb046dd3622be753234a5bc514dcb318
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-07-05 15:02:36 +0500

    created 3 dimensional arrays

--- dimensions_NP.py
+++ dimensions_NP.py
@@ -1,4 +1,27 @@
 import numpy as np
 
+# ZERO DIMESNIONAL ARRAYS
+
 array=np.array("A")
+print(array.ndim)
+
+# ONE DIMESNIONAL ARRAYS
+
+array=np.array(["A","B","C"])
+
+print(array.ndim)
+
+# TWO DIMENSIONAL ARRAYS
+
+array = np.array([["A","B","C"],
+                 ["D","E","F"],])
+
+print(array.ndim)
+
+# THREE DIMENSIONALS ARRAYS
+
+array = np.array([[["A","B"],
+                   ["C","D"],
+                   ["E","F"]]])
+
 print(array.ndim)
