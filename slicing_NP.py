19beca5259784f72fbf1daaa37d5bdbbe3229960
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-07-05 15:46:41 +0500

    USed slicing in arrays

---
+++ slicing_NP.py
@@ -0,0 +1,11 @@
+import numpy as np
+array_2d=np.array([["A","B","C","D"],
+    ["E","F","G","H"],
+    ["I","J","K","L"],
+    ["M","N","O","P"],
+    ["Q","R","S","T"],
+    ["U","V","W","X"]])
+
+
+
+print(array_2d[1:4])
