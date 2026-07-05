0c77bd936de66882f1809032a3718ae217621b27
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-07-05 15:56:26 +0500

    Added multiple slicing concepts

--- slicing_NP.py
+++ slicing_NP.py
@@ -1,5 +1,6 @@
 import numpy as np
-array_2d=np.array([["A","B","C","D"],
+array_2d=np.array([
+    ["A","B","C","D"],
     ["E","F","G","H"],
     ["I","J","K","L"],
     ["M","N","O","P"],
@@ -8,4 +9,17 @@ array_2d=np.array([["A","B","C","D"],
 
 
 
-print(array_2d[1:4])
+# print(array_2d[1:4])
+
+
+#ACCESSING 2nd-COLUMN only
+
+print(array_2d[:,1]) #: means selecting all rows. 1 means 1 element of each column, It starts from 0
+
+#ACCESSING 1st,2nd and 3rd column
+
+print(array_2d[:,0:3]) #0:3 means select from 0index to 3rd index
+
+#selcting "C,D" and "G,H"
+
+print(array_2d[0:2,2:4])
