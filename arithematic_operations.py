2764565e0882c2ea11b5a73bf9236bcd3de71433
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-07-06 19:26:11 +0500

    Understand how to get numbers from array

--- arithematic_operations.py
+++ arithematic_operations.py
@@ -1,11 +1,25 @@
 import numpy as np
-array1=np.array([1,2,3])
-array2=np.array([4,5,6])
+# array1=np.array([1,2,3])
+# array2=np.array([4,5,6])
 
-#Addition
-added_arrays=array1+array2
-print(added_arrays)
-#Subtration
-subtracted_arrays=array1-array2
-print(subtracted_arrays)
-# SImilar as Multiply and Divide and %
+# #Addition
+# added_arrays=array1+array2
+# print(added_arrays)
+# #Subtration
+# subtracted_arrays=array1-array2
+# print(subtracted_arrays)
+# # SImilar as Multiply and Divide and %
+
+#--------------------------------------------------------------------------------
+
+#Comparison Operators in Arrays
+
+scores=np.array([10,51,15,63,16,84,22])
+
+# print(scores>41)
+
+#selecting specific elements in array
+scores[scores < 50] 
+print(scores)
+#-----------------------------------------------------------------------------------
+
