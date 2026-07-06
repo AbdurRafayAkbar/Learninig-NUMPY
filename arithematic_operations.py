65adad068f91eb9df05a4bb3b6f10e03cc617f7a
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-07-06 19:10:06 +0500

    Learned About Arithematic Operatos

---
+++ arithematic_operations.py
@@ -0,0 +1,11 @@
+import numpy as np
+array1=np.array([1,2,3])
+array2=np.array([4,5,6])
+
+#Addition
+added_arrays=array1+array2
+print(added_arrays)
+#Subtration
+subtracted_arrays=array1-array2
+print(subtracted_arrays)
+# SImilar as Multiply and Divide and %
